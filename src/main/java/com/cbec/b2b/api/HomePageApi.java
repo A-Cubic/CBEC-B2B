88aa34e4c05dcc813233273c9b1802200e0499cc
ming han <40271842@qq.com>
2018-02-11 16:59:13 +0800

    添加个首页获取商品分类的方法

---
+++ src/main/java/com/cbec/b2b/api/HomePageApi.java
@@ -0,0 +1,31 @@
+package com.cbec.b2b.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cbec.b2b.entity.Demo;
+import com.cbec.b2b.entity.page.PageEntity;
+import com.cbec.b2b.entity.response.LoginResponseEntity;
+import com.cbec.b2b.service.IDemoService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+@RestController
+@RequestMapping(value = "/api")
+public class HomePageApi {
+    @Autowired
+    IDemoService service;
+    
+    @RequestMapping(value = "/catalog")
+    public String catalog() {
+        return "{\"state\":0,\"results\":{\"美容护肤\":[{\"id\":0,\"value\":\"面膜\"},{\"id\":1,\"value\":\"1\"}],\"level2\":[{\"catelog\":[{\"id\":0,\"value\":\"0\",\"childCate\":[{\"id\":0,\"value\":\"0\"}]}],\"brands\":[{\"id\":0,\"value\":\"0\"}]}]}}";
+    }
+    
+}
+
+
+
