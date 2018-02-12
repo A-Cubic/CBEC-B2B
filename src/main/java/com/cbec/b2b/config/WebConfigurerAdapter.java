69d4c312a122ca7c7b786eed1e02836ace88d4cc
Administrator <Administrator@WIN-01606221418>
2018-02-12 21:17:00 +0800

    跨域

--- src/main/java/com/cbec/b2b/config/WebConfigurerAdapter.java
+++ src/main/java/com/cbec/b2b/config/WebConfigurerAdapter.java
@@ -91,7 +91,10 @@ public class WebConfigurerAdapter extends WebMvcConfigurerAdapter {
     //解决跨域问题
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+        .allowedOrigins("http://console.llwell.net")
+        .allowedMethods("GET", "POST")
+        .allowCredentials(true).maxAge(3600);
     }
     
 //    @Override
