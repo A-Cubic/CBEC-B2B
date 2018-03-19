064c327083b0efba09be137365e9bf91564918a5
ming han <40271842@qq.com>
2018-03-19 18:00:41 +0800

    About #29 1

--- src/main/java/com/cbec/b2b/controller/HomePageController.java
+++ src/main/java/com/cbec/b2b/controller/HomePageController.java
@@ -1,5 +1,13 @@
 package com.cbec.b2b.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,6 +53,38 @@ public class HomePageController {
     @RequestMapping(value = "/Goods")
     public GoodsInfo goods(@RequestBody SearchGoods2 searchGoods) {
     	return api.getGoods(searchGoods.getGoodsId());
+    }
+
+    @RequestMapping(value = "/Phone")
+    public String Phone(@RequestBody Map<String,Object> request) {
+    	
+    	StringBuffer sb = new StringBuffer();
+    	   InputStreamReader isr = null;
+    	   BufferedReader br = null;
+    	   try
+    	   {
+    		 String code = URLEncoder.encode("#app#=流连物流&#code#=333242", "GBK").toLowerCase();  //输出%C4%E3%BA%C3
+    	     URL url = new URL("http://v.juhe.cn/sms/send?mobile=13644237400&tpl_id=65449&tpl_value="+code+"&key=7c21d791256af1ffdd85375c64846358");
+    	     URLConnection urlConnection = url.openConnection();
+    	     urlConnection.setAllowUserInteraction(false);
+    	     isr = new InputStreamReader(url.openStream());
+    	     br = new BufferedReader(isr);
+    	     String line;
+    	     while ((line = br.readLine()) != null)
+    	     {
+    	      sb.append(line);
+    	     }
+    	   }
+    	   catch (IOException e)
+    	   {
+    	     e.printStackTrace();
+    	   }
+    	   finally
+    	   {
+    	     
+    	   }
+    	   System.out.println(sb.toString());
+    	   return sb.toString();
     }
     
 }
